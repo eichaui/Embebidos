e82e0e6c19cf0d9d5f397d2dd166c5c508e2791e
eichaui <44421327+eichaui@users.noreply.github.com>
2019-01-15 13:44:44 -0500

    Update DEBER3_EEPROM.ino

--- SEGUNDO BIMESTRE/Ichau_Alexander_reloj/DEBER3_EEPROM.ino
+++ SEGUNDO BIMESTRE/Ichau_Alexander_reloj/DEBER3_EEPROM.ino
@@ -7,9 +7,9 @@
  *      MEMORIA EEPROM
  */
  #include <EEPROM.h>
- String v;
- int n;
- 
+ String v;//variable de dato a ingresar
+ int p;//variable posicion de la memoria 
+ int dato;//valor guardado en la memoria
 void setup() {
  Serial.begin(9600);  
   EEPROM.write(0, 0);
@@ -22,16 +22,18 @@ void loop() {
 }
 void memoria(){
     if (Serial.available() >= 0) {  //verifica si va a llegar dato por seria
-    Serial.print("Ingrese datos ");//impresion
+    Serial.println("Ingrese datos ");//impresion
     delay(200); 
     v = Serial.readString();  //lee el valor ingresado String
-    n = v.toInt();          //conversion String a int 
-    if (EEPROM.read(0) < n) { 
-      EEPROM.update(0, n); 
-      Serial.print("El valor es: ");  
-      Serial.println(EEPROM.read(0)); 
+    p= v.toInt();          //conversion String a int 
+     dato=EEPROM.read(0);   
+     if (dato < p) { 
+      EEPROM.update(0, p); //actualiza los valores de la memoria 
+      Serial.println("El valor es: ");  
+      Serial.println(EEPROM.read(0)); // imprime valor guardado en la memoria 
       delay(300);
-    } else {
+    } 
+     else {
       Serial.println("ERROR INGRESE DATO NUEVAMENTE"); //mensaje de advertencia
       delay(300);
     } } 
