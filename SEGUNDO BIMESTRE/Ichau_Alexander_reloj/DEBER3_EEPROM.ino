6ada7082320e0f8dd4f5a9aa0c87d812de07a4ed
eichaui <44421327+eichaui@users.noreply.github.com>
2019-01-15 11:40:44 -0500

    Update DEBER3_EEPROM.ino

--- SEGUNDO BIMESTRE/Ichau_Alexander_reloj/DEBER3_EEPROM.ino
+++ SEGUNDO BIMESTRE/Ichau_Alexander_reloj/DEBER3_EEPROM.ino
@@ -7,32 +7,33 @@
  *      MEMORIA EEPROM
  */
  #include <EEPROM.h>
+ String v;
+ int n;
  
 void setup() {
-  // put your setup code here, to run once:
+ Serial.begin(9600);  
+  EEPROM.write(0, 0);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  memoria();
 
 }
 void memoria(){
-    if (Serial.available() >= 0) {  //comprobar si hay algo en la CX serial
-    Serial.print("Ingrese el valor a guardar entre 0 y 255: "); //mensaje de informacion
-    delay(500); 
-    var = Serial.readString();  //Tomar valor del monitor serie
-    num = var.toInt();          //convertir string a int
-    if (EEPROM.read(0) < num) { //condicion para guardar informacion
-      EEPROM.update(0, num);    //actualizar la EEPROM en la posicion 0
-      Serial.print("El valor guardado en la EEPROM es: ");  //mensaje de confirmacion
-      Serial.println(EEPROM.read(0)); //Impresion por serial del valor en la EEPROM
-      delay(100);
+    if (Serial.available() >= 0) {  //verifica si va a llegar dato por seria
+    Serial.print("Ingrese datos ");//impresion
+    delay(200); 
+    v = Serial.readString();  //lee el valor ingresado String
+    n = v.toInt();          //conversion String a int 
+    if (EEPROM.read(0) < n) { 
+      EEPROM.update(0, n); 
+      Serial.print("El valor es: ");  
+      Serial.println(EEPROM.read(0)); 
+      delay(300);
     } else {
-      Serial.println("No puede ingresar letras o el valor es igual o menor al guardado. "); //mensaje de advertencia
-      delay(100);
-    }
-  }
-  
+      Serial.println("ERROR INGRESE DATO NUEVAMENTE"); //mensaje de advertencia
+      delay(300);
+    } } 
   
   }
