2d69bdb4f47b7d15d4e5bc328089c566ac52071b
eichaui <44421327+eichaui@users.noreply.github.com>
2019-02-05 08:57:14 -0500

    Add files via upload

---
+++ SEGUNDO BIMESTRE/Ichau_Alexander_reloj/DEBER MODO SLEEP/Ichau_Alex_modoSleep/deber_reloj/deber_reloj.ino
@@ -0,0 +1,20 @@
+/*
+ * UNIVERSIDAD TECNICA DEL NORTE 
+ *    FICA CIERCOM
+ *   ALEXANDER ICHAU
+ *   
+ *   
+ *   Realizar un sistema que permita configurar por 
+comunicación serial o por pulsadores el modo sleep cada 25 segundos.
+
+ */
+ int a;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
